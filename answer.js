a0aa651031fad29bbbf42390e2125df039169f9d
Mahadi Hashan <mahadi333h@gmail.com>
2026-09-02 23:36:05 +0600

    add 4th

--- answer.js
+++ answer.js
@@ -57,4 +57,14 @@ function validateUsername(string){
 // Question 4: Dhaka CNG Fare Meter
 
 
-
+function getCngFare(distance, isNight = false, waitingMinutes =0){
+    let fare = 50;
+    if(distance >2){
+        fare += (distance - 2) * 15;
+    }
+    fare += waitingMinutes * 2;
+     if (isNight) {
+        fare = fare * 1.20;
+    }
+    return fare;
+}
